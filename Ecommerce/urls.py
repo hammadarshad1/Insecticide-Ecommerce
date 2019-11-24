5f30a84dba3f3e35fd93d79a4e03adee603d5c33
Muhammad Hammad <hammadarshad834@gmail.com>
2019-11-24 18:46:51 +0500

    Added Search Functionality

--- Ecommerce/urls.py
+++ Ecommerce/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name="Home"),
+    path('', views.ProductsListView.as_view(), name="Home"),
     path('product/<pk>',views.product_view, name='Product-View'),
     path('about/', views.about, name='About'),
-    path('product/search/', views.searchProduct, name='Search')
+    path('product/search/', views.searchProduct, name='Search'),
+    path('404/', views.err404, name='Error404'),
+    path('product/search/not-found', views.errNotFound, name='Pr-Not-Found')
 ]
--- Ecommerce/views.py
+++ Ecommerce/views.py
@@ -1,17 +1,26 @@
 from django.shortcuts import render, redirect
 from . import models
+from django.views.generic import ListView
+import mysql.connector
 
 # Create your views here.
 
-def home(request):
-    if request.method == "POST":
-        searchText = request.POST.get("search")
-        print(searchText)
-        # searchProduct(request, searchProduct)
-        # return redirect('Search')
-    products = models.Products.objects.all()
-    print(products)
-    return render(request, 'Ecommerce/home.html', {'products': products})
+# def home(request):
+#     if request.method == "POST":
+#         searchText = request.POST.get("search")
+#         print(searchText)
+#         # searchProduct(request, searchProduct)
+#         # return redirect('Search')
+#     products = models.Products.objects.all()
+#     print(products)
+#     return render(request, 'Ecommerce/home.html', {'products': products})
+
+conn = mysql.connector.connect(user='admin', host='localhost', port='3306', password='Gre@ter834', database="Ecommerce")
+
+class ProductsListView(ListView):
+    model = models.Products
+    template_name = 'Ecommerce/home.html'
+    paginate_by = 20
 
 def product_view(request, pk):
     product = models.Products.objects.get(productId=pk)
@@ -22,4 +31,35 @@ def about(request):
     return render(request, 'Ecommerce/about.html')
 
 def searchProduct(request):
-    return render(request, 'Ecommerce/search.html')
+    if request.method == "POST":
+        Search = request.POST.get('search')
+        print(Search)
+        # string = input('Enter Search: ')
+        a = Search.split(' ')
+        query="""select * from Ecommerce_products where productTitle LIKE """
+
+        for li in a:
+            if a[-1] == li:
+                query += "'%{}%' OR '%{}' OR '{}%' ".format(li, li, li)
+                # a.pop(a.index(li))
+            else:
+                query += "'%{}%' OR '{}%' OR '%{}' AND ".format(li, li, li)
+                # a.pop(a.index(li))
+        print(query)
+        try:
+            cursor = conn.cursor()
+            query = cursor.execute(query)
+            query = cursor.fetchall()
+        except:
+            return redirect('Error404')
+        if not query:
+            return redirect('Pr-Not-Found')
+            
+        print(query)
+    return render(request, 'Ecommerce/search.html', {'search':query})
+
+def err404(request):
+    return render(request, 'Ecommerce/404.html')
+
+def errNotFound(request):
+    return render(request, 'Ecommerce/pr-not-found.html')
